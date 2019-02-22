123f3ebd2909f49d3a625e1c8d72b193cea9b76a
jhwan7 <jhwan7@uwo.ca>
2019-02-22 14:21:47 -0500

    Part1
    Hero, Scene, Border

---
+++ SE2250_Phase1/Assets/__Scripts/BoundsCheck.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Keeps GameObject on Screen
+/// </summary>
+
+public class BoundsCheck : MonoBehaviour
+{
+    
+    public float radius = 1f;
+
+    public float camWidth;
+    public float camHeight;
+
+    private void Awake()
+    {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        if(pos.x > camWidth - radius)
+        {
+            pos.x = camWidth - radius;
+        }
+
+        if(pos.x < -camWidth + radius)
+        {
+            pos.x = -camWidth + radius;
+        }
+        if(pos.y >camHeight - radius)
+        {
+            pos.y = camHeight - radius;
+        }
+        if(pos.y<-camHeight + radius)
+        {
+            pos.y = -camHeight + radius;
+        }
+        transform.position = pos;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return;
+        Vector3 boundSize = new Vector3(camWidth * 2, camHeight * 2, 0.1f);
+        Gizmos.DrawWireCube(Vector3.zero, boundSize);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SE2250_Phase1/Assets/__Scripts/Hero.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    static public Hero S;
+
+    public float speed = 30f;
+    public float rollMult = -45f;
+    public float pitchMult = 30f;
+
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if(S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("Hero.Aware() - Attempted to assign second Hero.S!");
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+        
+    }
+}
